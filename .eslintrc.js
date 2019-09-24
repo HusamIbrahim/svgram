1ef1ac02084a17a451a5e34ffc98e128be02a54f
Husam Ibrahim <huielbashir@gmail.com>
2019-09-25 01:38:52 +0300

    🚨 fix linting errors

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,30 +1,30 @@
 module.exports = {
   root: true,
   env: {
-    node: true
+    node: true,
   },
-  extends: ["plugin:vue/recommended", "@vue/prettier"],
+  extends: ['plugin:vue/recommended', '@vue/prettier'],
   rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
-    "prettier/prettier": [
-      "error",
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'prettier/prettier': [
+      'error',
       {
-        "trailingComma": "es5",
-        "semi": false,
-        "singleQuote": true,
-      }
-    ], 
+        trailingComma: 'es5',
+        semi: false,
+        singleQuote: true,
+      },
+    ],
   },
   parserOptions: {
-    parser: "babel-eslint"
+    parser: 'babel-eslint',
   },
   overrides: [
     {
-      files: ["**/__tests__/*.{j,t}s?(x)"],
+      files: ['**/__tests__/*.{j,t}s?(x)'],
       env: {
-        jest: true
-      }
-    }
-  ]
-};
+        jest: true,
+      },
+    },
+  ],
+}
