8529f7f4737b55386d8fe9e26640aa1866db22ec
Husam Ibrahim <huielbashir@gmail.com>
2019-11-03 01:23:45 +0300

    🔧 migrate to typescript

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,12 @@
 module.exports = {
   root: true,
+
   env: {
     node: true,
   },
+
   extends: ['plugin:vue/recommended', '@vue/prettier'],
+
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
@@ -16,9 +19,11 @@ module.exports = {
       },
     ],
   },
+
   parserOptions: {
-    parser: 'babel-eslint',
+    parser: '@typescript-eslint/parser',
   },
+
   overrides: [
     {
       files: ['**/__tests__/*.{j,t}s?(x)'],
@@ -27,4 +32,10 @@ module.exports = {
       },
     },
   ],
+
+  'extends': [
+    'plugin:vue/recommended',
+    '@vue/prettier',
+    '@vue/typescript'
+  ]
 }
--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,28 @@
 <template>
-  <div></div>
+  <div id="app">
+    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  </div>
 </template>
 
-<script>
-export default {
+<script lang="ts">
+import Vue from 'vue'
+import HelloWorld from './components/HelloWorld.vue'
+
+export default Vue.extend({
   name: 'App',
-  components: {},
-}
+  components: {
+    HelloWorld,
+  },
+})
 </script>
 
-<style></style>
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
