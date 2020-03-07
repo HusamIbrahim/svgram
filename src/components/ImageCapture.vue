844cfbef39a4c8dffa843fd90830e4a0748c1a7f
Husam Ibrahim <huielbashir@gmail.com>
2020-03-08 01:38:04 +0200

    💩 scaffold image capture component

---
+++ src/components/ImageCapture.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <form @submit.prevent>
+      <input type="file" accept="image/*" capture @change="handleChange" />
+    </form>
+    <img ref="img" />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from '@vue/composition-api'
+
+export default defineComponent({
+  setup() {
+    const img = ref(null)
+
+    const handleChange = (e: InputEvent) => {
+      console.log(e)
+    }
+
+    return { img, handleChange }
+  },
+})
+</script>
+
+<style></style>
