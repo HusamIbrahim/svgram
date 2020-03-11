54dd725cfda138b111638702fa7ffef5c5a428ef
Husam Ibrahim <huielbashir@gmail.com>
2020-03-11 11:49:50 +0200

    🚧 continue work on image capture component
    Extract selected file and add image to DOM

--- src/components/ImageCapture.vue
+++ src/components/ImageCapture.vue
@@ -8,14 +8,25 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, ref } from '@vue/composition-api'
+import { defineComponent, Ref, ref } from '@vue/composition-api'
 
 export default defineComponent({
   setup() {
-    const img = ref(null)
+    const img = ref<HTMLImageElement>(null)
 
     const handleChange = (e: InputEvent) => {
-      console.log(e)
+      const fileList = (e.target as HTMLInputElement).files
+
+      if (fileList) {
+        const file = fileList[0]
+
+        if (img.value) {
+          const objectURL = URL.createObjectURL(file)
+          img.value.src = objectURL
+          // release object URL after image load since it's no longer needed
+          img.value.onload = () => URL.revokeObjectURL(objectURL)
+        }
+      }
     }
 
     return { img, handleChange }
